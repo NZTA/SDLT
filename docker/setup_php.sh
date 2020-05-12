fd61671a731f32c90c7d203158cdf1633e1aeab6
Scott Rasmussen (Zaita) <scott@zaita.com>
2020-05-12 21:47:40 +1200

    MINOR updating docker setup script and .env.example
    - .env.example defaults to live to stop broken login with PHP warnings
    - docker/setup.php removed composer install as it can't install

--- docker/setup_php.sh
+++ docker/setup_php.sh
@@ -4,10 +4,8 @@ apt-get -y install libicu-dev
 docker-php-ext-install intl 
 docker-php-ext-install mysqli
 docker-php-ext-install pdo_mysql  
-apt-get -y install composer
-composer install
-php vendor/silverstripe/framework/cli-script.php dev/build
-./vendor/bin/sake dev/tasks/HydrateCustomConfig
+php /var/www/html/vendor/silverstripe/framework/cli-script.php dev/build
+/var/www/html/vendor/bin/sake dev/tasks/HydrateCustomConfig
 docker-php-entrypoint php-fpm
 
 
